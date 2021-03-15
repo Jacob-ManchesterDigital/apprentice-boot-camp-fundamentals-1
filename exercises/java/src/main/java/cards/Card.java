66524534e2961a3b42a2a4e97f9ee580d236310e
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-03-15 15:41:56 +0000

    Move card suit & value conversion logic into Card class

--- exercises/java/src/main/java/cards/Card.java
+++ exercises/java/src/main/java/cards/Card.java
@@ -1,16 +1,39 @@
 package cards;
 
 public class Card {
-    String faceValue;
-    String suit;
+    String faceValueString;
+    String suitString;
 
-    public Card(String faceValue, String suit) {
-        this.faceValue = faceValue;
-        this.suit = suit;
+    public Card(int faceValue, int suit) {
+        switch (faceValue){
+            case 0: this.faceValueString = "ace"; break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9: this.faceValueString = Integer.toString(faceValue+1); break;
+            case 10: this.faceValueString = "jack"; break;
+            case 11: this.faceValueString = "queen"; break;
+            case 12: this.faceValueString = "king"; break;
+            default: throw new IllegalArgumentException("Something went wrong " + faceValue + "is not a valid faceValue!");
+        }
+        switch (suit){
+            case 0: this.suitString = "clubs"; break;
+            case 1: this.suitString = "diamonds"; break;
+            case 2: this.suitString = "hearts"; break;
+            case 3: this.suitString = "spades"; break;
+            default: throw new IllegalArgumentException("Something went wrong " + suit + "is not a valid suitName!");
+        }
     }
 
     @Override
     public String toString() {
-        return faceValue + " of " + suit;
+        return faceValueString + " of " + suitString;
     }
+
+
 }
