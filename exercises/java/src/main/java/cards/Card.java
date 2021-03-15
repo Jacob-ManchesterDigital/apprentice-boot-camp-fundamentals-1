c09e25511fa91e25cc4d657d00f631023952aaeb
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-03-15 16:19:53 +0000

    Move conversion of card suit & face value into new methods

--- exercises/java/src/main/java/cards/Card.java
+++ exercises/java/src/main/java/cards/Card.java
@@ -5,8 +5,26 @@ public class Card {
     String suitString;
 
     public Card(int faceValue, int suit) {
+        this.faceValueString = convertFaceValueString(faceValue);
+        this.suitString =  convertSuitString(suit);
+    }
+
+    private String convertSuitString(int suit) {
+        String suitString;
+        switch (suit){
+            case 0: suitString = "clubs"; break;
+            case 1: suitString = "diamonds"; break;
+            case 2: suitString = "hearts"; break;
+            case 3: suitString = "spades"; break;
+            default: throw new IllegalArgumentException("Something went wrong " + suit + "is not a valid suitName!");
+        }
+        return suitString;
+    }
+
+    private String convertFaceValueString(int faceValue) {
+        String faceValueString;
         switch (faceValue){
-            case 0: this.faceValueString = "ace"; break;
+            case 0: faceValueString = "ace"; break;
             case 1:
             case 2:
             case 3:
@@ -15,25 +33,17 @@ public class Card {
             case 6:
             case 7:
             case 8:
-            case 9: this.faceValueString = Integer.toString(faceValue+1); break;
-            case 10: this.faceValueString = "jack"; break;
-            case 11: this.faceValueString = "queen"; break;
-            case 12: this.faceValueString = "king"; break;
+            case 9: faceValueString = Integer.toString(faceValue +1); break;
+            case 10: faceValueString = "jack"; break;
+            case 11: faceValueString = "queen"; break;
+            case 12: faceValueString = "king"; break;
             default: throw new IllegalArgumentException("Something went wrong " + faceValue + "is not a valid faceValue!");
         }
-        switch (suit){
-            case 0: this.suitString = "clubs"; break;
-            case 1: this.suitString = "diamonds"; break;
-            case 2: this.suitString = "hearts"; break;
-            case 3: this.suitString = "spades"; break;
-            default: throw new IllegalArgumentException("Something went wrong " + suit + "is not a valid suitName!");
-        }
+        return faceValueString;
     }
 
     @Override
     public String toString() {
         return faceValueString + " of " + suitString;
     }
-
-
 }
