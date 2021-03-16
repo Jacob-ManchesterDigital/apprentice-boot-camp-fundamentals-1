444283a166289bd89c86249e111b5e181df80624
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-03-16 11:55:10 +0000

    Create Deck interface and wire into AnimalDeck

--- exercises/java/src/main/java/cards/AnimalDeck.java
+++ exercises/java/src/main/java/cards/AnimalDeck.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class AnimalDeck {
+class AnimalDeck implements Deck {
 
     private List<AnimalCard> cards;
 
@@ -16,11 +16,13 @@ class AnimalDeck {
         }
     }
 
-    void shuffle() {
+    @Override
+    public void shuffle() {
         Collections.shuffle(cards);
     }
 
-    String[] getCards() {
+    @Override
+    public String[] getCards() {
         String[] result = new String[cards.size()];
         for (int i = 0; i < cards.size(); i++) {
             AnimalCard card = cards.get(i);
@@ -29,7 +31,8 @@ class AnimalDeck {
         return result;
     }
 
-    AnimalCard deal() {
+    @Override
+    public Card deal() {
         return cards.remove(0);
     }
 }
